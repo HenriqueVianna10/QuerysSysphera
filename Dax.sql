acdd234ce73de70968fab4aa9845165505653166
Henrique Vianna <henriquev79@gmail.com>
2020-08-31 22:39:25 -0300

    power query e dax

---
+++ Dax.sql
@@ -0,0 +1,8 @@
+Codigo_Exp2 = [Produto] & [Serial number]
+
+CONCATENATE([Produto],[Serial number])
+
+Margem = 1 - (DIVIDE([Preço Custo], [Valor de Venda],0))
+
+Media Vendas = AVERAGE(Custos[Valor de Venda])
+
