19e3895cdaeb3c5014fd06bf276c59b7ef26e33c
Henrique Vianna <henriquev79@gmail.com>
2021-08-04 17:11:05 -0300

    dicas e dicas

---
+++ updatesazureaad114.sql
@@ -0,0 +1,5 @@
+update REP_PARAMETER set dscParameter = '' where  codParameter = 'AADClientId'
+update REP_PARAMETER set dscParameter = '' where  codParameter = 'AADDomain'
+update REP_PARAMETER set dscParameter = '' where  codParameter = 'AADTenantId'
+update REP_PARAMETER set dscParameter = '' where  codParameter = 'AADUrlLogin'
+update REP_PARAMETER set dscParameter = '' where  codParameter = 'AADUrlLogout'
