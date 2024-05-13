3d2560c1cbfc916b8f91b4d6bdffa4f657400d36
Henrique Vianna <hvianna@tech6group.com>
2024-05-13 15:55:07 -0300

    Query para update de licença

---
+++ QueryUpdateLicenca.sql
@@ -0,0 +1,31 @@
+update REP_LICENSE set dscLicense = '<?xml version="1.0"?>
+<LicenseVO xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <Name>Hospital S�o Camilo - HSC</Name>
+  <Company>SOCIEDADE BENEFICENTE SAO CAMILO</Company>
+  <Type>TEMPORARY</Type>
+  <LicenseVersion>11.10</LicenseVersion>
+  <Deadline>2024-08-31</Deadline>
+  <Signature>Q4zZI5ytt/LYZdA8Bc1371RKAXDauCnRPuIsebSXaphSBQOH5Fo2D/aq836xgFd5wfd7fIaoYRc7Qn+Dfnnpmoh5XRFgJaDjm1CIyYW26UXmSDYItZFkRXz5nuWtr7fY1nfQB673gJmo0+eKoXcyFadU0MDuHcI1P0jdmlgBpHw=</Signature>
+  <UsersLicense>4000</UsersLicense>
+  <UsersLicenseConsolidation>4000</UsersLicenseConsolidation>
+  <UsersLicenseDataWarehouse>0</UsersLicenseDataWarehouse>
+  <Excel>true</Excel>
+  <ExcelType>PERMANENTLY</ExcelType>
+  <ExcelDeadline>2099-12-31</ExcelDeadline>
+  <Monitor>true</Monitor>
+  <MonitorType>PERMANENTLY</MonitorType>
+  <MonitorDeadline>2099-12-31</MonitorDeadline>
+  <UserLimitsAdmin>1000</UserLimitsAdmin>
+  <UserLimitsManager>1000</UserLimitsManager>
+  <UserLimitsAnalyst>2000</UserLimitsAnalyst>
+  <UserLimitsPlanner>0</UserLimitsPlanner>
+  <UserLimitsAdminConsolidation>1000</UserLimitsAdminConsolidation>
+  <UserLimitsManagerConsolidation>1000</UserLimitsManagerConsolidation>
+  <UserLimitsAnalystConsolidation>2000</UserLimitsAnalystConsolidation>
+  <UserLimitsPlannerConsolidation>0</UserLimitsPlannerConsolidation>
+  <UserLimitsAdminDataWarehouse>0</UserLimitsAdminDataWarehouse>
+  <UserLimitsManagerDataWarehouse>0</UserLimitsManagerDataWarehouse>
+  <UserLimitsAnalystDataWarehouse>0</UserLimitsAnalystDataWarehouse>
+  <UserLimitsPlannerDataWarehouse>0</UserLimitsPlannerDataWarehouse>
+  <CryptographyKey>F8BC71185EFD257109D9243A7908AE47A02EE385246D0B36</CryptographyKey>
+</LicenseVO>' where codVersion = '11.10'
