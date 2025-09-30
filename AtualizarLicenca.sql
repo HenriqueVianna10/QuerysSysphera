8887b3c9748e50abbcf3f2f7dc8cf4137ef1d504
Henrique Martim da Silva Vianna <hvianna@tech6group.com>
2025-09-30 16:36:23 -0300

    Atualizar Licenca Manual

---
+++ AtualizarLicenca.sql
@@ -0,0 +1,31 @@
+update REP_LICENSE set dscLicense = '<?xml version="1.0"?>
+<LicenseVO xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <Name>Hospital São Camilo - HSC</Name>
+  <Company>SOCIEDADE BENEFICENTE SAO CAMILO</Company>
+  <Type>TEMPORARY</Type>
+  <LicenseVersion>11.10</LicenseVersion>
+  <Deadline>2026-08-31</Deadline>
+  <Signature>Gx2NvPQopHXE+89dw7rjxVqOu3uVI7Dy0F4g/TcjRX9BULNhOxgbIrxI77yeCJu0IhTZWH7NJ3I6+RdiJGcGZxDEheEq1m+LsVMhD6sABK0L3pHjRFcWWhbisYVwU0PoPN0OkHfHvGTUne8uMHF8J5UvuMr7fqkFQZGs6bjzymE=</Signature>
+  <UsersLicense>4000</UsersLicense>
+  <UsersLicenseConsolidation>4000</UsersLicenseConsolidation>
+  <UsersLicenseDataWarehouse>0</UsersLicenseDataWarehouse>
+  <Excel>true</Excel>
+  <ExcelType>PERMANENTLY</ExcelType>
+  <ExcelDeadline>2099-12-31</ExcelDeadline>
+  <Monitor>true</Monitor>
+  <MonitorType>PERMANENTLY</MonitorType>
+  <MonitorDeadline>2099-12-31</MonitorDeadline>
+  <UserLimitsAdmin>1000</UserLimitsAdmin>
+  <UserLimitsManager>1000</UserLimitsManager>
+  <UserLimitsAnalyst>2000</UserLimitsAnalyst>
+  <UserLimitsPlanner>0</UserLimitsPlanner>
+  <UserLimitsAdminConsolidation>1000</UserLimitsAdminConsolidation>
+  <UserLimitsManagerConsolidation>1000</UserLimitsManagerConsolidation>
+  <UserLimitsAnalystConsolidation>2000</UserLimitsAnalystConsolidation>
+  <UserLimitsPlannerConsolidation>0</UserLimitsPlannerConsolidation>
+  <UserLimitsAdminDataWarehouse>0</UserLimitsAdminDataWarehouse>
+  <UserLimitsManagerDataWarehouse>0</UserLimitsManagerDataWarehouse>
+  <UserLimitsAnalystDataWarehouse>0</UserLimitsAnalystDataWarehouse>
+  <UserLimitsPlannerDataWarehouse>0</UserLimitsPlannerDataWarehouse>
+  <CryptographyKey>F8BC71185EFD257109D9243A7908AE47A02EE385246D0B36</CryptographyKey>
+</LicenseVO>' where codVersion = '11.10'
