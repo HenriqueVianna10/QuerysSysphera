3cbf265410c9befde5bad9ad190556f66be6e692
Henrique Martim da Silva Vianna <hvianna@tech6group.com>
2025-11-17 10:56:27 -0300

    Ajustes Query

--- Gerandoinsertparaoutrobanco.sql
+++ Gerandoinsertparaoutrobanco.sql
@@ -4,3 +4,19 @@ inner join d_conta_app2 d on f.sk_conta = d.sk_conta
 where f.sk_tempo = 139
 and f.sk_cenario = 80
 and d.sk_conta_l1 = 425
+
+
+
+
+
+SELECT 'INSERT INTO DT_t6fg_depara_contas(cod_user, dat_update, sk_conta, data_inicial, data_final, sk_conta_dre, sk_conta_bp, sk_conta_dva, sk_conta_decof) VALUES('''
+       + ISNULL(CAST(cod_user AS NVARCHAR(20)), 'NULL') + ''','
+       + ISNULL('CONVERT(DATETIME, ''' + CONVERT(VARCHAR(23), dat_update, 121) + ''', 121)', 'NULL') + ','
+       + ISNULL(CAST(sk_conta AS NVARCHAR(20)), 'NULL') + ','
+       + ISNULL('CONVERT(DATETIME, ''' + CONVERT(VARCHAR(23), data_inicial, 121) + ''', 121)', 'NULL') + ','
+       + ISNULL('CONVERT(DATETIME, ''' + CONVERT(VARCHAR(23), data_final, 121) + ''', 121)', 'NULL') + ','
+       + ISNULL(CAST(sk_conta_dre AS NVARCHAR(20)), 'NULL') + ','
+       + ISNULL(CAST(sk_conta_bp AS NVARCHAR(20)), 'NULL') + ','
+       + ISNULL(CAST(sk_conta_dva AS NVARCHAR(20)), 'NULL') + ','
+       + ISNULL(CAST(sk_conta_decof AS NVARCHAR(20)), 'NULL') + ')'
+FROM DT_t6fg_depara_contas
